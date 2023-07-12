50e8226ec29f7facc446d088a43cefd8515d8049
zahfir <zafy2001@gmail.com>
2023-07-12 12:14:37 -0400

    Basic input page and header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,37 @@
 "use client";
-import { Button, Grid, Stack } from "@mui/material";
+import { Button, Grid, Stack, TextField } from "@mui/material";
 export default function Home() {
   return (
-    <Grid container height="100vh" alignItems="center" justifyContent="center" direction="column">
-      <h1>Using Material UI with Next.js 13</h1>
-      <Stack direction="row" columnGap={1}>
-        <Button variant="text">Text</Button>
-        <Button variant="contained">Contained</Button>
-        <Button variant="outlined">Outlined</Button>
-      </Stack>
+    <Grid container alignItems="center" justifyContent="center" direction="row">
+      <h1 className="text-3xl">Coveroo</h1>
+      <Grid
+        container
+        alignItems="center"
+        justifyContent="center"
+        direction="column"
+      >
+        <h1 className="text-xl">Enter Resume Text:</h1>
+        {/* Text Fields and Button */}
+        <Grid
+          container
+          width="100vh"
+          alignItems="center"
+          justifyContent="center"
+          direction="column"
+        >
+          <TextField multiline minRows={10} fullWidth label="Resume" />
+          <TextField
+            sx={{ marginTop: "2%" }}
+            fullWidth
+            label="Enter Job Application URL"
+          />
+          <Stack direction="row" columnGap={1}>
+            <Button sx={{ marginTop: "20%" }} variant="outlined">
+              Generate Cover Letter
+            </Button>
+          </Stack>
+        </Grid>
+      </Grid>
     </Grid>
   );
 }
