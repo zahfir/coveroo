fb258e038d2b4108a1ac3b607be123397cfb4ce1
zahfir <zafy2001@gmail.com>
2023-07-24 16:02:16 -0400

    Text field onchange listeners

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 "use client";
 import { Button, Grid, Stack, TextField } from "@mui/material";
+import { useState } from "react";
+import Link from "next/link";
+
 export default function Home() {
+  const [resume, setResume] = useState("");
+  const [job, setJob] = useState("");
+
   return (
     <Grid container alignItems="center" justifyContent="center" direction="row">
       <h1 className="text-3xl">Coveroo</h1>
@@ -19,16 +25,30 @@ export default function Home() {
           justifyContent="center"
           direction="column"
         >
-          <TextField multiline minRows={10} fullWidth label="Resume" />
+          <TextField
+            multiline
+            minRows={10}
+            fullWidth
+            label="Resume"
+            onChange={(e) => setResume(e.target.value)}
+          />
           <TextField
             sx={{ marginTop: "2%" }}
             fullWidth
             label="Enter Job Application URL"
+            onChange={(e) => setJob(e.target.value)}
           />
           <Stack direction="row" columnGap={1}>
-            <Button sx={{ marginTop: "20%" }} variant="outlined">
-              Generate Cover Letter
-            </Button>
+            <Link
+              href={{
+                pathname: "/CoverLetter",
+                query: { resume, job },
+              }}
+            >
+              <Button sx={{ marginTop: "20%" }} variant="outlined">
+                Generate Cover Letter
+              </Button>
+            </Link>
           </Stack>
         </Grid>
       </Grid>
